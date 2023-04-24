ccce04ac404cbf07a87e6ab8e07f4bc032401eec
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2023-04-24 06:36:19 +0200

    restaurantApp

---
+++ restaurantApp/android/app/src/main/kotlin/start4tec/com/resturant_app_fluttert/MainActivity.kt
@@ -0,0 +1,12 @@
+package start4tec.com.resturant_app_fluttert
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
